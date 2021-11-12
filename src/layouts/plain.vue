52ac701ebcd1a72daecdfa2c1ce7d51c3f40ecf0
kthatoto <kthatoto@gmail.com>
2021-11-13 03:34:58 +0900

    FIx

---
+++ src/layouts/plain.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="layout">
+    <nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif
+}
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  box-sizing: border-box;
+}
+html,
+body,
+#__nuxt {
+  height: 100%;
+  background-color: #444;
+}
+
+.layout {
+  background-color: #fff;
+  min-height: 100vh;
+  max-width: 414px;
+  margin: 0 auto;
+}
+</style>
